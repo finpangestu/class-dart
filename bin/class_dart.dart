dced3e7d82080209d64f8446898c270650be2dd7
finpangestu <195410015@students.akakom.ac.id>
2022-08-09 20:07:59 +0700

    implementation inheritance

--- bin/class_dart.dart
+++ bin/class_dart.dart
@@ -1,5 +1,6 @@
 // import 'package:class_dart/class_dart.dart' as class_dart;
 import 'Animal.dart';
+import 'Cat.dart';
 
 void main() {
   Animal('', 2, 4.2)
@@ -7,5 +8,11 @@ void main() {
   ..eat()
   ..poop()
   ..sleep();
-  // print(dicodingCat.weight);
+
+  print('');
+
+  var dicodingCat = Cat('Grayson', 2, 2.2, 'Gray');
+  dicodingCat.walk();
+  dicodingCat.eat();
+  print(dicodingCat.weight);
 }
